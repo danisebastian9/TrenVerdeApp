303c3b7746c107ec66ccb3f5a8f3f260b51d7cf0
FelipeAndresLopez <felipe.lopez@uptc.edu.co>
2024-05-09 14:30:36 -0500

    configuración base datos, creación proyecto en springboot, configuración conexión DB y creación docker-compose.

---
+++ src/main/java/TrenVerdeApp/TrenVerdeApp/TrenVerdeAppApplication.java
@@ -0,0 +1,13 @@
+package TrenVerdeApp.TrenVerdeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrenVerdeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrenVerdeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TrenVerdeApp/TrenVerdeApp/TrenVerdeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package TrenVerdeApp.TrenVerdeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrenVerdeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
